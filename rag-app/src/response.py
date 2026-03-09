51eb0c1a75c642584ad50e6524f224e5dffb1ead
Natalie <nataliem2336@gmail.com>
2026-03-09 17:44:26 +0100

    API call for LLM generated response

--- rag-app/src/response.py
+++ rag-app/src/response.py
@@ -1,12 +1,18 @@
-
+from dotenv import load_dotenv
 from openai import OpenAI
+from semantic_search import semantic_search
 import os
-api_key = os.getenv("OPENAI_API_KEY")
-client = OpenAI()
 
-response = client.responses.create(
-    model = "gpt-4o-mini",
-    input = "hello"
-)
+load_dotenv()
 
-print(response.output_text)
+def generate_response():
+    query, results = semantic_search()
+    context = "\n\n".join(r["text"] for r in results)
+    prompt = f"context: {context} \nprompt: {query}"
+    client = OpenAI()
+    response = client.responses.create(
+        model = "gpt-4o-mini",
+        input = prompt)
+    print(f"LLM response: \n{response.output_text}")
+
+response = generate_response()
--- rag-app/src/semantic_search.py
+++ rag-app/src/semantic_search.py
@@ -7,7 +7,6 @@ model = SentenceTransformer("all-MiniLM-L6-v2")
 def semantic_search():
     query = input("Search for legal and privacy data: ")
     embedded_query = model.encode(query)
-    top_k = []
     while True:
         try: 
             k = int(input("How many results would you like to return? "))
@@ -20,21 +19,10 @@ def semantic_search():
         cos_similarity = cosine_similarity([embedded_query], [d["embedding"]])[0][0] #cosine_similarity returns 1x1 matrix of a score
         d["cos_score"] = cos_similarity.tolist()
     ranked = sorted(data, key=lambda data: data["cos_score"], reverse=True) 
-    for i in range(k):
-        top_k.append(ranked[i])
-    result_rank = 0
-    for each_k in top_k:
-        result_rank += 1
-        source = each_k["source"]
-        text = each_k["text"]
-        print(f"Result: {result_rank}\nScore: {cos_similarity} \nSource: {source} \nText: {text}")
-        
-retrieve = semantic_search()
-print(retrieve)  
-
-client = OpenAI()
-
-response = client.responses.create(
-    model = "gpt-5.4",
-    input = "hi"
-)
+    top_k = ranked[:k]
+    # for rank, each_k in enumerate(top_k, start = 1):
+    #     print(f"\nResult: {rank}")
+    #     print(f"\nScore: {cos_similarity}")
+    #     print(f"\nSource: {each_k["source"]}")
+    #     print(f"\nText: {each_k["text"]}") 
+    return query, top_k
